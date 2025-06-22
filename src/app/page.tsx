05036a19676638725d886e4c8afe54482e6b58e5
unknown <rabyaan.m@gmail.com>
2025-06-22 23:33:16 +0500

    first commit s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api"; 
 import { Authenticated, Unauthenticated } from 'convex/react'
